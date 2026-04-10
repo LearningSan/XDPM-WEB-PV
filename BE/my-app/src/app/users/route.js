bcdece7947d6feb8958dbe2828cbc13b62a70eca
Only-San <duysannguyen113@gmail.com>
2026-04-10 13:11:29 +0700

    Delete BE/my-app directory

--- BE/my-app/src/app/users/route.js
+++
@@ -1,8 +0,0 @@
-import clientPromise from "../../lib/mongodb";
-
-export async function GET() {
-    const client= await clientPromise
-    const db=client.db("dbTest1")
-    const users=await db.collection("User").find().toArray()
-    return Response.json(users)
-}
